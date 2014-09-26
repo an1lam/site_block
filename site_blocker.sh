16e9e682946c960f52d2727b0132d8dd059f7951
krazemon <stephenmalina@gmail.com>
2014-09-26 11:42:58 -0400

    Make Posix compliant

--- site_blocker.sh
+++ site_blocker.sh
@@ -1,14 +1,7 @@
 SITES_FILE=$2
 ARG=$1
-echo "Ran program" >> ./job.log
-ROOT_UID="0"
-#Check if run as root
-if [ "$UID" -ne "$ROOT_UID" ] ; then
-  echo "You must be root to do that!"
-  exit 1
-fi
 
-function block_sites {
+block_sites () {
   while read line;
   do
     echo "0.0.0.0 $line" >> /etc/hosts
@@ -16,21 +9,21 @@ function block_sites {
   done < $1
 }
 
-function unblock_sites {
+unblock_sites () {
   sed -i -e '/0.0.0.0/d' /etc/hosts 
 }
 
 
-function usage {
+usage () {
   echo "Usage: ./toggle_block_sites [ -b|u ]"
 }
 
-if [ "$ARG" == "-b" ]
+if [ "$ARG" = "-b" ]
 then
   block_sites $SITES_FILE
-elif [ "$ARG" == "-u" ]
+elif [ "$ARG" = "-u" ]
 then
-  unblock_sites 
+  unblock_sites
 else
   usage;
   exit 1;
